2490cca5e1ddb3681d87a46dd1a133cb6ceb9fb6
Cedric Aebi <cedric.aebi@me.com>
2024-08-02 13:45:24 +0200

    initial commit

---
+++ apryse-ios/ios-app/ios-app/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ios-app
+//
+//  Created by Cedric Aebi on 31.07.2024.
+//
+
+import UIKit
+import PDFNet
+import Tools
+
+class ViewController: UIViewController, PTReflowViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Get the path to document in the app bundle.
+        let pdfPath: String? = Bundle.main.path(forResource: "HYG-157", ofType: "pdf")
+        
+        // Instantiate a new PDFDoc with the path to the file.
+        let docToOpen = PTPDFDoc(filepath: pdfPath)!
+
+        // Create a PTDocumentController
+        let documentController = PTDocumentController()
+        // The PTDocumentController must be in a navigation controller before a document can be opened
+        let navigationController = UINavigationController(rootViewController: documentController)
+        navigationController.modalPresentationStyle = .fullScreen
+        navigationController.navigationBar.isTranslucent = false
+        navigationController.toolbar.isTranslucent = false
+        // Open a file from URL.
+        let fileURL: URL = URL(string:"https://www.fess.ie/images/stories/ResourcesForTutors/Referencing_Handbook_files/DiagramsFiguresImagesTables_58-59.pdf")!
+        documentController.openDocument(with: docToOpen)
+        // Show navigation (and document) controller.
+        self.present(navigationController, animated: true, completion: nil)
+    }
+}
+
---
+++ pspdfkit-flutter/flutter_application_1/lib/src/sample_feature/sample_item_details_view.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:pspdfkit_flutter/pspdfkit.dart';
+
+/// Displays detailed information about a SampleItem.
+class SampleItemDetailsView extends StatelessWidget {
+  const SampleItemDetailsView({super.key});
+
+  Future<String> extractAsset(BuildContext context, String assetPath) async {
+    if (kIsWeb) {
+      return assetPath;
+    }
+
+    final bytes = await DefaultAssetBundle.of(context).load(assetPath);
+    final list = bytes.buffer.asUint8List();
+    final tempDir = await Pspdfkit.getTemporaryDirectory();
+    final tempDocumentPath = '${tempDir.path}/$assetPath';
+    final file = File(tempDocumentPath);
+
+    await file.create(recursive: true);
+    file.writeAsBytesSync(list);
+    return file.path;
+  }
+
+  static const routeName = '/sample_item';
+  static const documentPath = 'assets/pdfs/sensors-23-03984-with-cover.pdf';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Item Details'),
+      ),
+      body: FutureBuilder<String>(
+          future: extractAsset(context, documentPath),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              /// PspdfkitWidget is a widget that displays a PDF document.
+              return PspdfkitWidget(
+                documentPath: snapshot.data!,
+                configuration: PdfConfiguration(
+                    iOSRightBarButtonItems: ['readerViewButtonItem']),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
