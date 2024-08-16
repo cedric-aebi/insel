9b5b3a1ac37880c4dfcd8b549b67a7c294a681df
Cedric Aebi <cedric.aebi@me.com>
2024-08-16 12:47:24 +0200

    added pwa support

--- begehungen/frontend/vite.config.ts
+++ begehungen/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 import svgr from "vite-plugin-svgr"
 import eslintPlugin from "@nabla/vite-plugin-eslint"
 import checker from "vite-plugin-checker"
+import { VitePWA } from "vite-plugin-pwa"
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig(({ mode }) => {
@@ -29,6 +30,31 @@ export default defineConfig(({ mode }) => {
       eslintPlugin(),
       // svgr options: https://react-svgr.com/docs/options/
       svgr({ svgrOptions: { icon: true } }),
+      VitePWA({
+        registerType: "autoUpdate",
+        devOptions: {
+          enabled: true,
+        },
+        includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+        manifest: {
+          name: "Insel Spitalhygiene - Begehungen",
+          short_name: "Begehungen",
+          description: "Insel Spitalhygiene - Begehungen",
+          theme_color: "#009870",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
     ],
   }
 })
